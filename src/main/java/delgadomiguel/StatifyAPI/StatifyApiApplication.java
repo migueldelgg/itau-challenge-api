ffdb0c2e36b84d07e3a64777daa41e860a6a5d12
Miguel Delgado <delgdevoleper@gmail.com>
2025-03-26 21:52:20 -0300

    repositório criado

---
+++ src/main/java/delgadomiguel/StatifyAPI/StatifyApiApplication.java
@@ -0,0 +1,13 @@
+package delgadomiguel.StatifyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatifyApiApplication.class, args);
+	}
+
+}
